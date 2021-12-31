40109516af7dcfa56f6bb3e152740c1b380ea328
Abelardo Zavala <azavalacoria@icloud.com>
2021-12-31 08:48:49 -0600

    file to openconnection. to fix: NegativeArraySizeException on request

--- app/src/main/java/com/azavalacoria/testfiles/MainActivity.kt
+++ app/src/main/java/com/azavalacoria/testfiles/MainActivity.kt
@@ -11,26 +11,29 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.os.storage.StorageVolume
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.azavalacoria.testfiles.databinding.ActivityMainBinding
-import java.io.File
+import com.google.android.material.snackbar.Snackbar
+import java.io.*
+import java.net.URL
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var list: Array<String>
     private lateinit var permissionsToAsk: ArrayList<String>
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -55,7 +58,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-        val list = arrayOf(
+        list = arrayOf(
             Manifest.permission.INTERNET,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -93,9 +96,10 @@ class MainActivity : AppCompatActivity() {
                     val builder = AlertDialog.Builder(this)
                     builder.setTitle("Result")
                     builder.setMessage("All permissions granted")
-                    builder.setPositiveButton("Ok", DialogInterface.OnClickListener { dialog, id ->
+                    builder.setPositiveButton("Ok") { dialog, id ->
                         Log.d("TAG", "$id")
-                    })
+                        currentPermissions(list)
+                    }
                     builder.show()
                 } else {
                     Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
@@ -121,16 +125,19 @@ class MainActivity : AppCompatActivity() {
                 ActivityCompat.requestPermissions(this@MainActivity, array, 1)
             }
         } else {
-            downloadFile("xd")
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                downloadOpenConnection("master")
+            } else {
+                Log.d("TAG", "No hay permiso")
+            }
         }
     }
 
     @SuppressLint("Range", "ResourceType")
     private fun downloadFile(id: String) {
-        val serverUrl = resources.getStringArray(R.string.url_server)
+        val serverUrl = resources.getString(R.string.url_server)
         val url = "$serverUrl/$id"
-        val directory = this.cacheDir
-        //File(Environment.DIRECTORY_DOWNLOADS)
+        val directory = File(Environment.DIRECTORY_DOWNLOADS)
         val downloadManager = this.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
         val downloadUri = Uri.parse(url)
         val request = DownloadManager.Request(downloadUri).apply {
@@ -138,8 +145,7 @@ class MainActivity : AppCompatActivity() {
                 .setAllowedOverRoaming(false)
                 .setTitle(url.substring(url.lastIndexOf("/") + 1))
                 .setDescription("")
-                .setDestinationInExternalFilesDir(
-                    this@MainActivity,
+                .setDestinationInExternalPublicDir(
                     directory.toString(),
                     url.substring(url.lastIndexOf("/") + 1)
                 )
@@ -165,37 +171,55 @@ class MainActivity : AppCompatActivity() {
                 }
                 cursor.close()
             }
-            val tempFile = File(directory, id)
-            if (tempFile.exists()) {
-                Log.d("TAG", "exists")
+            //val path = "${getStoragePath()}${Environment.DIRECTORY_DOWNLOADS}/master.zip"
+            val streamPath =  getFileStreamPath("master.zip")
+            if (streamPath.exists()) {
+                streamPath.unzip()
             } else {
                 Log.d("TAG", "not exista")
-            }
-            val files = directory.listFiles();
-            Log.d("TAG", "$files.size")
-            for (f in files) {
-                Log.d("TAG", f.absolutePath)
-            }
-            /*
-            val sv = getExternalFilesDir(null)
-            val splited = sv?.absolutePath?.split("Android")
-            if (splited?.size!! > 1) {
-                val full = "${splited[0]}Downloads/$id"
-                var downloadedFile = File(full)
-                Log.d("TAG", "Absolute file ${downloadedFile.absolutePath}")
-
-                if (downloadedFile.exists()) {
-                    Log.d("TAG", "Existe")
-                } else {
-                    Log.d("TAG", "no Existe")
+                val files = directory.listFiles()
+                Log.d("TAG", "$files.size")
+                for (f in files) {
+                    Log.d("TAG", f.absolutePath)
                 }
             }
-            */
+
+        }.start()
+    }
+
+    private fun downloadOpenConnection(id: String) {
+        Thread {
+            try {
+                val serverUrl = resources.getString(R.string.url_server)
+                val mainUrl = "$serverUrl/$id"
+                val url = URL(mainUrl)
+                val connection = url.openConnection()
+                val contentLength = connection.contentLength
+                val stream = DataInputStream(url.openStream())
+                val buffer = ByteArray(contentLength)
+                stream.readFully(buffer)
+                stream.close()
+                val file = File.createTempFile(id, ".zip")
+                val fos = FileOutputStream(file)
+                val dos = DataOutputStream(fos)
+                dos.write(buffer)
+                dos.flush()
+                dos.close()
+                if (file.exists()) {
+                    val path = "${getStoragePath()}${Environment.DIRECTORY_DOWNLOADS}"
+                    val dirFile = File(path)
+                    Log.d("TAG", "file size: ${file.length()}")
+                    file.unzip(dirFile)
+                }
+            } catch (error: Exception) {
+                error.printStackTrace()
+                Log.d("TAG", error.localizedMessage)
+            }
         }.start()
     }
 
     private fun statusMessage(url: String, directory: File, status: Int): String? {
-        var msg = ""
+        var msg: String
         msg = when (status) {
             DownloadManager.STATUS_FAILED -> "Download has been failed, please try again"
             DownloadManager.STATUS_PAUSED -> "Paused"
@@ -209,11 +233,46 @@ class MainActivity : AppCompatActivity() {
         return msg
     }
 
-    private fun checkFile() {
-        val file = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            Environment.getStorageDirectory()
+    private fun getStoragePath(): String {
+        val splited = getExternalFilesDir(null)?.absolutePath?.split("Android")
+        return if (splited?.size!! > 1) {
+            splited[0]
         } else {
-            TODO("VERSION.SDK_INT < R")
+            ""
         }
     }
+}
+
+data class ZipIO (val entry: ZipEntry, val output: File)
+
+fun File.unzip(unzipLocationRoot: File? = null) {
+
+    val rootFolder = unzipLocationRoot ?: File( parentFile.absolutePath + File.separator + "unzip-" + nameWithoutExtension)
+    if (!rootFolder.exists()) {
+        rootFolder.mkdirs()
+    }
+    ZipFile(this).use { zip ->
+        zip
+            .entries()
+            .asSequence()
+            .map {
+                val outputFile = File(rootFolder.absolutePath + File.separator + it.name)
+                ZipIO(it, outputFile)
+            }
+            .map {
+                it.output.parentFile?.run{
+                    if (!exists()) mkdirs()
+                }
+                it
+            }
+            .filter { !it.entry.isDirectory }
+            .forEach { (entry, output) ->
+                zip.getInputStream(entry).use { input ->
+                    output.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+            }
+    }
+
 }
